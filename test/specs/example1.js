27fd99c9ce22956e90bce197a8ca8cda9f398244
Jerry <jerry.osorio@autoweb.com>
2021-08-15 21:22:09 -0600

    example 1

--- test/specs/example1.js
+++ test/specs/example1.js
@@ -1,7 +1,7 @@
 describe("webdrive.io page", () => {
-  it("should have the right tile", () => {
-    browser.url("https://webdriver.io");
-    expect(browser).toHaveTitle(
+  it("should have the right tile", async () => {
+    await browser.url("https://webdriver.io");
+    await expect(browser).toHaveTitle(
       "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO"
     );
   });
