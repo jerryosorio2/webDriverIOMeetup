40463ab28d3d7e0a3c06d7231827c05eb0c6056d
Jerry <jerry.osorio@autoweb.com>
2021-08-28 19:41:17 -0600

    reseting project

--- test/specs/example1.js
+++
@@ -1,8 +0,0 @@
-describe("webdrive.io page", () => {
-  it("should have the right tile", async () => {
-    await browser.url("https://webdriver.io");
-    await expect(browser).toHaveTitle(
-      "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO"
-    );
-  });
-});
